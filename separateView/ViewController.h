1b44a87367921cdba954958fc77a1de62308cc19
Jonghyun Kim <filmhomage@hotmail.com>
2014-06-08 22:05:00 +0900

    Initial Commit

---
+++ separateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  separateView
+//
+//  Created by earth on 6/8/14.
+//  Copyright (c) 2014 filmhomage.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
